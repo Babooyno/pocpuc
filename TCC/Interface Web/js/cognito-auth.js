91c65c7692822870a2ddd8a047b5653138bfe96d
Felipe Torres da Silva <felipe.torres.silva@hotmail.com>
2020-06-14 20:49:51 -0300

    Mudança de pasta

---
+++ TCC/Interface Web/js/cognito-auth.js
@@ -0,0 +1,95 @@
+var SGQ6 = window.SGQ6 || {};
+
+// Funções de manipulação da chave de autenticação
+(function scopeWrapper($) {
+    var signinUrl = 'signin.html';
+
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
+
+    var userPool;
+
+    if (!(_config.cognito.userPoolId &&
+          _config.cognito.userPoolClientId &&
+          _config.cognito.region)) {
+        $('#noCognitoMessage').show();
+        return;
+    }
+
+    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    if (typeof AWSCognito !== 'undefined') {
+        AWSCognito.config.region = _config.cognito.region;
+    }
+
+    SGQ6.signOut = function signOut() {
+        userPool.getCurrentUser().signOut();
+    };
+
+    SGQ6.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+        var cognitoUser = userPool.getCurrentUser();
+
+        if (cognitoUser) {
+            cognitoUser.getSession(function sessionCallback(err, session) {
+                if (err) {
+                    reject(err);
+                } else if (!session.isValid()) {
+                    resolve(null);
+                } else {
+                    resolve(session.getIdToken().getJwtToken());
+                }
+            });
+        } else {
+            resolve(null);
+        }
+    });
+
+    // Função para validar autenticação de usuário e senha.
+    function signin(email, password, onSuccess, onFailure) {
+        var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+            Username: email,
+            Password: password
+        });
+
+        // Função de autenticação de usuário
+        var cognitoUser = createCognitoUser(email);
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: onSuccess,
+            onFailure: onFailure
+        });
+    }
+
+    //Cria usuário para validação
+    function createCognitoUser(email) {
+        return new AmazonCognitoIdentity.CognitoUser({
+            Username: email,
+            Pool: userPool
+        });
+    }
+
+    /*
+     *  Eventos
+     */
+
+    $(function onDocReady() {
+        $('#signinForm').submit(handleSignin);
+    });
+
+    //Valida usuário inserido na página e redireciona para aplicação.
+    function handleSignin(event) {
+        var email = $('#emailInputSignin').val();
+        var password = $('#passwordInputSignin').val();
+        event.preventDefault();
+        signin(email, password,
+            function signinSuccess() {  
+                console.log('Successfully Logged In');
+                window.location.href = 'sgq.html';
+            }, 
+            function signinError(err) {
+                alert(err);
+            }
+        );
+    }  
+}(jQuery));
---
+++ TCC/Interface Web/js/sgq6.js
@@ -0,0 +1,80 @@
+var SGQ6 = window.SGQ6 || {};
+
+//Verifica se o token foi recebido para redirecionar para a página de login
+(function rideScopeWrapper($) {
+    var authToken;
+    SGQ6.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/signin.html';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/signin.html';
+    });
+
+//Função de envio de parâmetros para calcular KPI
+    function sendKPI() {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeUrl + '/kpi-post',
+            headers: {
+                Authorization: authToken //Token recebido AWS Cognito
+            },
+            data: JSON.stringify({ //Parâmetros
+                KPI: {
+                    p1: $('#param1').val(),
+                    p2: $('#param2').val(),
+                    p3: $('#param3').val(),
+                    p4: $('#param4').val(),
+                    p5: $('#param5').val(),
+                    p6: $('#param6').val(),
+                    p7: $('#param7').val(),
+                    p8: $('#param8').val(),
+                    p9: $('#param9').val(),
+                    p10: $('#param10').val(),
+                }
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                alert('Um erro ocorreu na requisição:\n' + jqXHR.responseText);
+            }
+        });
+    }
+
+    // Função de exibição dos dados retornados.
+    function completeRequest(result) {        
+        displayUpdate('KPI 1: ' + result.KPI1 + ' | KPI 2: ' + result.KPI2 + ' | KPI 3: ' + result.KPI3 + ' | KPI 4: ' + result.KPI4 + ' | KPI 5: ' + result.KPI5 + ' | KPI 6: ' + result.KPI6 + ' | KPI 7: ' + result.KPI7);  
+    }
+
+    // Handler de requisição dos KPIs e de Logout da aplicação
+    $(function onDocReady() {
+        $('#request').click(handleRequestClick);
+        $('#signOut').click(function() {
+            SGQ6.signOut();
+            alert("Voce foi deslogado.");
+            window.location = "signin.html";
+        });
+
+        if (!_config.api.invokeUrl) { // Caso não exista parametros no config
+            $('#noApiMessage').show();
+        }
+
+        $('#auth').click(function () { //Mostra token
+            displayUpdate(authToken);
+        });
+    });
+
+    // Handler de requisição de enio
+    function handleRequestClick(event) {
+        event.preventDefault();        
+        sendKPI();         
+    }
+
+    //Função de display em tela.
+    function displayUpdate(text) {
+        $('#updates').append($('<li>' + text + '</li>'));
+    }
+}(jQuery));
